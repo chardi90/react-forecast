2888d0b8d23986d35fb1103af908b25a90ee8968
chardi90 <chardi90@hotmail.com>
2025-07-07 17:21:09 +0100

    Reorganised weather css & fixed footer styling

--- src/Footer.js
+++ src/Footer.js
@@ -3,35 +3,34 @@ import "./Footer.css";
 
 export default function Footer() {
   return (
-    <footer className="footer">
-      Coded by
-      <a
-        href="https://chardi-portfolio.netlify.app/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        {" "}
-        Clare Harding
-      </a>
-      , open-sourced on
-      <a
-        href="https://github.com/chardi90/react-forecast"
-        target="_blank"
-        rel="noreferrer"
-      >
-        {" "}
-        GitHub
-      </a>
-      , and
-      <a
-        href="https://forecast-on-react.netlify.app/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        {" "}
-        hosted on Netlify
-      </a>
-      .
-    </footer>
+    <div className="Footer">
+      <p>
+        Coded by{" "}
+        <a
+          href="https://chardi-portfolio.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Clare Harding
+        </a>
+        , open-sourced on{" "}
+        <a
+          href="https://github.com/chardi90/react-forecast"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+        , and{" "}
+        <a
+          href="https://forecast-on-react.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          hosted on Netlify
+        </a>
+        .
+      </p>
+    </div>
   );
 }
