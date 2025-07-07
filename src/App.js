dbbf9c7bb0dc52b8e828131b3f390ae527f618b0
chardi90 <chardi90@hotmail.com>
2025-07-07 18:10:11 +0100

    Styled and tweaked margins
    - Added margin to top of app
    - Added fahrenheit buttons to header
    - imported axios to weather.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Weather from "./Weather";
 
 export default function App() {
   return (
-    <div className="App">
+    <div className="App container">
       <header className="App-header">
         <Header />
       </header>
--- src/Header.js
+++ src/Header.js
@@ -4,16 +4,28 @@ import "./Header.css";
 export default function Header() {
   return (
     <div className="Header">
-      <form>
-        <input
-          type="search"
-          name="search-input"
-          placeholder="Enter a city..."
-          className="search-input"
-          autoFocus="on"
-        />
-        <input type="submit" value="Search" className="search-button" />
-      </form>
+      <div className="row d-flex">
+        <div className="col-2 variables d-flex flex-column justify-content-start">
+          <button type="button" className="btn btn-units">
+            °F
+          </button>
+          <button type="button" className="btn btn-units mt-1">
+            M/h
+          </button>
+        </div>
+        <div className="col-10 d-flex justify-content-end">
+          <form>
+            <input
+              type="search"
+              name="search-input"
+              placeholder="Enter a city..."
+              className="search-input"
+              autoFocus="on"
+            />
+            <input type="submit" value="Search" className="search-button" />
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
